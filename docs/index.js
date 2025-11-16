03a4a3cc3dbf7d9cb5d955bc1600a51c29ff78a7
it-is-zane <zane.gant@gmail.com>
2025-11-15 23:23:18 -0800

    included missing Math Module and removed unused node.js file

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,4 @@
-const importObj = {};
+const importObj = { Math };
 
 const response = await fetch("bin/web_asm.wasm");
 const {instance} = await WebAssembly.instantiateStreaming(response, importObj);
